e368522cd04e82de21e33626e038e75ffad73d95
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-02-28 15:53:04 +0100

    Adicionando guarda segura

---
+++ frontend/sso-linear/src/app/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private oauthService: OAuthService) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const hasIdToken = this.oauthService.hasValidIdToken();
+    const hasAccessToken = this.oauthService.hasValidAccessToken();
+
+    return (hasIdToken && hasAccessToken);
+  }
+
+}
