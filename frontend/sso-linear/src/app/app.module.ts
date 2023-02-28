ceb5889812f8c3883d131a4cf4458a663221f903
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-02-28 15:13:12 +0100

    Criando o componente de Welcome

--- frontend/sso-linear/src/app/app.module.ts
+++ frontend/sso-linear/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
